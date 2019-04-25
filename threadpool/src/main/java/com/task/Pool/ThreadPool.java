249371326cce25cc7bfb9efaee5b14df8756ff2c
178625120 <178625120@qq.com>
2019-04-25 12:53:00 +0800

    ailibaba手册线程池使用

---
+++ threadpool/src/main/java/com/task/Pool/ThreadPool.java
@@ -0,0 +1,86 @@
+package com.task.Pool;
+
+import java.io.IOException;
+import java.util.concurrent.*;
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * @author wangqianlong
+ * @create 2019-04-25 12:31
+ */
+
+public class ThreadPool {
+    public static void main(String[] args) throws IOException {
+        int corePoolSize = 5;
+        int maximumPoolSize = 8;
+        long keepAliveTime = 10;
+        TimeUnit unit = TimeUnit.SECONDS;
+        BlockingQueue<Runnable> workQueue = new ArrayBlockingQueue<>(5);
+        ThreadFactory threadFactory = new NameTreadFactory();
+        RejectedExecutionHandler handler = new ThreadPoolExecutor.CallerRunsPolicy();
+        ThreadPoolExecutor executor = new ThreadPoolExecutor(corePoolSize, maximumPoolSize, keepAliveTime, unit,
+                workQueue, threadFactory, handler);
+        executor.prestartAllCoreThreads(); // 预启动所有核心线程coreSize
+
+        for (int i = 1; i <= 100; i++) {
+            MyTask task = new MyTask(String.valueOf(i));
+            executor.execute(task);
+        }
+
+        System.in.read(); //阻塞主线程
+    }
+
+    static class NameTreadFactory implements ThreadFactory {
+
+        private final AtomicInteger mThreadNum = new AtomicInteger(1);
+
+        @Override
+        public Thread newThread(Runnable r) {
+            Thread t = new Thread(r, "my-thread-" + mThreadNum.getAndIncrement());
+            System.out.println(t.getName() + " has been created");
+            return t;
+        }
+    }
+
+    /*public static class MyIgnorePolicy implements RejectedExecutionHandler {
+
+        public void rejectedExecution(Runnable r, ThreadPoolExecutor e) {
+            doLog(r, e);
+        }
+
+        private void doLog(Runnable r, ThreadPoolExecutor e) {
+            // 可做日志记录等
+            System.err.println(r.toString() + " rejected");
+//          System.out.println("completedTaskCount: " + e.getCompletedTaskCount());
+        }
+    }*/
+
+    static class MyTask implements Runnable {
+        private String name;
+
+        public MyTask(String name) {
+            this.name = name;
+        }
+
+        @Override
+        public void run() {
+            try {
+                System.out.println(this.toString() + " is running!");
+                Thread.sleep(3000); //让任务执行慢点
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        @Override
+        public String toString() {
+            return "MyTask [name=" + name + "]";
+        }
+    }
+
+}
+
