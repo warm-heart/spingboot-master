7e0d2bdd38a258c824eb1a2f513307d5f234dc7c
178625120 <178625120@qq.com>
2019-08-04 20:38:59 +0800

    springboot 自定义starter

---
+++ wqlspringbootstarter/src/main/java/com/study/service/HelloService.java
@@ -0,0 +1,16 @@
+package com.study.service;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * @author wangqianlong
+ * @create 2019-08-04 20:02
+ */
+@Component
+public class HelloService {
+
+    public String sayHello(){
+        return "hello";
+    }
+
+}
---
+++ wqlspringbootstarter/src/main/java/com/study/service/HelloServiceAutoConfiguration.java
@@ -0,0 +1,13 @@
+package com.study.service;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author wangqianlong
+ * @create 2019-08-04 20:04
+ */
+@Configuration
+@ComponentScan({"com.study.service"})
+public class HelloServiceAutoConfiguration {
+}
