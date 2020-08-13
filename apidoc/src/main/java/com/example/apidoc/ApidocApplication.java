e8246738e273ea9b987c2e6ea46bf33016655bbd
178625120 <178625120@qq.com>
2020-08-13 21:25:54 +0800

    # 项目结构调整

---
+++ apidoc/src/main/java/com/example/apidoc/ApidocApplication.java
@@ -0,0 +1,13 @@
+package com.example.apidoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApidocApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApidocApplication.class, args);
+    }
+
+}
---
+++ apidoc/src/test/java/com/example/apidoc/ApidocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.apidoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApidocApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
